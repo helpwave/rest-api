d3294eacf00b9ddf76dcc996ab89e064b24d3d97
Friedjof Noweck <60787233+Friedjof@users.noreply.github.com>
2022-10-01 20:32:51 +0200

    docs(5): fix postgresql_url
    Co-authored-by: Max Baumann <max@bmn.dev>

--- migrate.sh
+++ migrate.sh
@@ -3,6 +3,6 @@
 # dotenv
 export $(egrep -v '^#' .env | xargs)
 
-POSTGRESQL_URL="postgres://$POSTGRES_USER:$POSTGRES_PASSWORD@$DB_HOST:$DB_PORT/$POSTGRES_DB?sslmode=disable"
+POSTGRESQL_URL="postgres://$POSTGRES_USER:$POSTGRES_PASSWORD@$POSTGRES_HOST:$POSTGRES_PORT/$POSTGRES_DB?sslmode=disable"
 
 migrate -database $POSTGRESQL_URL -path db/migrations $1
