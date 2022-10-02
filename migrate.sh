f1421bcd23ef79cdfb8070cab0a3bc32d57544dd
Max Baumann <max@bmn.dev>
2022-10-02 18:09:12 +0200

    feat: update migrate.sh

--- migrate.sh
+++ migrate.sh
@@ -5,4 +5,4 @@ export $(egrep -v '^#' .env | xargs)
 
 POSTGRESQL_URL="postgres://$POSTGRES_USER:$POSTGRES_PASSWORD@$POSTGRES_HOST:$POSTGRES_PORT/$POSTGRES_DB?sslmode=disable"
 
-migrate -database $POSTGRESQL_URL -path db/migrations $1
+migrate -database $POSTGRESQL_URL -path db/migrations "$@"
