da823f86f823621f5a4b87026b691258c6a21b83
Max Baumann <max@bmn.dev>
2022-10-16 15:20:30 +0200

    feat(#36): add additional check

--- routes/auth.go
+++ routes/auth.go
@@ -4,6 +4,7 @@ import (
 	"errors"
 	"github.com/gin-gonic/gin"
 	"net/http"
+	"regexp"
 	"rest-api/logging"
 )
 
@@ -12,11 +13,29 @@ func AuthMiddleware() gin.HandlerFunc {
 		log, _ := logging.GetRequestLogger(ctx)
 		authHeader := ctx.GetHeader("Authorization")
 
+		regex := regexp.MustCompile("^Bearer (\\w+)$")
+
+		var err error
+		var token string
+
 		if len(authHeader) == 0 {
-			err := errors.New("missing Authorization header")
+			err = errors.New("missing Authorization header")
+		} else {
+			matches := regex.FindStringSubmatch(authHeader)
+			if len(matches) != 2 {
+				err = errors.New("authorization header invalid")
+			} else {
+				token = matches[1]
+			}
+		}
+
+		if err != nil {
 			log.Warn().Err(err).Send()
 			SendError(ctx, http.StatusUnauthorized, err)
 			return
 		}
+
+		// TODO: validate token and set user
+		ctx.Set("authToken", token)
 	}
 }
